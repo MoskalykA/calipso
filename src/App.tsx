1756dca584a9d749c3089880de54e3c89733bb25
MoskalykA <streamarthur462@gmail.com>
2022-06-08 07:43:15 +0200

    style: removal of semicolons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import * as React from "react";
-import {Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
+import {Route, Routes} from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
         </Routes>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import * as React from "react";
-
 function Home() {
     return (
         <div>
             <h1>Home</h1>
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Home
