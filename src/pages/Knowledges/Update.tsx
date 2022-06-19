dd0a1c7a063a749b95c957b36d53a6de44c4baf5
MoskalykA <streamarthur462@gmail.com>
2022-06-19 21:20:12 +0200

    refactor: design rework of input

--- src/pages/Knowledges/Update.tsx
+++ src/pages/Knowledges/Update.tsx
@@ -31,41 +31,43 @@ function Update() {
     })
 
     return (
-        <div className="bg-zinc-800/50 m-4 h-screen rounded-md cool-border flex justify-between flex-col">
-            <div className="m-4 space-y-4">
-                <div>
-                    <h1 className="font-mono">Name</h1>
-                    <input ref={refName} onChange={(e) => {
-                        setName(e.target.value)
-                    }} className="bg-zinc-900 cool-border rounded font-mono text-white" type="text"/>
+        <div className="flex justify-center items-center h-screen">
+            <div className="bg-zinc-800/50 w-11/12 rounded-md cool-border flex flex-col">
+                <div className="m-4 space-y-4">
+                    <div>
+                        <h1 className="font-mono">Name</h1>
+                        <input ref={refName} onChange={(e) => {
+                            setName(e.target.value)
+                        }} className="p-1 w-full bg-zinc-900 cool-border rounded font-mono text-white focus:outline-0" type="text"/>
+                    </div>
+
+                    <div>
+                        <h1 className="font-mono">Image</h1>
+                        <input ref={refImage} onChange={(e) => {
+                            setImage(e.target.value)
+                        }} className="p-1 w-full bg-zinc-900 cool-border rounded font-mono text-white focus:outline-0" type="url"/>
+                    </div>
+
+                    <div>
+                        <h1 className="font-mono">Link</h1>
+                        <input ref={refLink} onChange={(e) => {
+                            setLink(e.target.value)
+                        }} className="p-1 w-full bg-zinc-900 cool-border rounded font-mono text-white focus:outline-0" type="url"/>
+                    </div>
                 </div>
 
-                <div>
-                    <h1 className="font-mono">Image</h1>
-                    <input ref={refImage} onChange={(e) => {
-                        setImage(e.target.value)
-                    }} className="bg-zinc-900 cool-border rounded font-mono text-white" type="url"/>
+                <div className="flex justify-center items-center">
+                    <Link to={"/knowledges/index"} onClick={() => {
+                        invoke("update_knowledges", {
+                            id: Number(id),
+                            name,
+                            image,
+                            link
+                        })
+                    }} className="button-header font-mono mb-4 px-4">
+                        <h1>Update</h1> 
+                    </Link>
                 </div>
-
-                <div>
-                    <h1 className="font-mono">Link</h1>
-                    <input ref={refLink} onChange={(e) => {
-                        setLink(e.target.value)
-                    }} className="bg-zinc-900 cool-border rounded font-mono text-white" type="url"/>
-                </div>
-            </div>
-
-            <div className="flex justify-center items-center">
-                <Link to={"/knowledges/index"} onClick={() => {
-                    invoke("update_knowledges", {
-                        id: Number(id),
-                        name,
-                        image,
-                        link
-                    })
-                }} className="button-header font-mono mb-4">
-                    <h1>Update</h1> 
-                </Link>
             </div>
         </div>
     )
